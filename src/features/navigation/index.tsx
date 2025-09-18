d94c69f6dc16d4b344b5e8cbfaf2f8a33cf2b0a7
Мдфв <v.v.titerez@gmail.com>
2025-09-18 22:19:12 +0300

    Fixed navbar

--- src/features/navigation/index.tsx
+++ src/features/navigation/index.tsx
@@ -1,5 +1,5 @@
 import NavLink from './components/NavLink';
-import {NavItem} from './types/NavItem';
+import { NavItem } from './types/NavItem';
 import {
   User,
   Settings,
@@ -15,16 +15,16 @@ import Logout from './components/Logout';
 
 const navSections: NavItem[][] = [
   [
-    {href: '/', label: 'Go to main', icon: House},
-    {href: '/profile', label: 'Profile', icon: User},
-    {href: '/settings', label: 'Settings', icon: Settings},
+    { href: '/', label: 'Go to main', icon: House },
+    { href: '/profile', label: 'Profile', icon: User },
+    { href: '/settings', label: 'Settings', icon: Settings },
   ],
   [
-    {href: '/students', label: 'Students', icon: Users2},
-    {href: '/professors', label: 'Professors', icon: BookOpen},
-    {href: '/clubs', label: 'Clubs', icon: Layers},
-    {href: '/channels', label: 'All Channels', icon: Layers},
-    {href: '/create-channel', label: 'Create Channel', icon: PlusCircle},
+    { href: '/students', label: 'Students', icon: Users2 },
+    { href: '/professors', label: 'Professors', icon: BookOpen },
+    { href: '/clubs', label: 'Clubs', icon: Layers },
+    { href: '/channels', label: 'All Channels', icon: Layers },
+    { href: '/create-channel', label: 'Create Channel', icon: PlusCircle },
   ],
   [
     {
@@ -44,7 +44,7 @@ const navSections: NavItem[][] = [
 
 export default function Navbar() {
   return (
-    <nav className='flex h-screen basis-1/4 flex-col justify-center gap-15 border-r pl-[8%] text-[#818181]'>
+    <nav className='fixed flex h-screen w-[25%] flex-col justify-center gap-15 border-r pl-[8%] text-[#818181]'>
       {navSections.map((section, sectionIndex) => (
         <div key={sectionIndex} className='flex flex-col gap-5'>
           {section.map((item, index) => (
