5d8d7f63beea8e36d96bde7fcb44b3af23c68600
Andre Chicu <andriesh.chicu@gmail.com>
2025-09-23 19:55:03 +0300

    err type

--- src/app/api/auth/confirm/route.ts
+++ src/app/api/auth/confirm/route.ts
@@ -1,28 +1,37 @@
-import {type EmailOtpType} from '@supabase/supabase-js';
-import {type NextRequest} from 'next/server';
+import { type EmailOtpType } from '@supabase/supabase-js';
+import { type NextRequest } from 'next/server';
 
-import {createClient} from '@/utils/supabase/server';
-import {redirect} from 'next/navigation';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import db from '@/db';
+import { users } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 export async function GET(request: NextRequest) {
-  const {searchParams} = new URL(request.url);
-  const token_hash = searchParams.get('token_hash');
-  const type = searchParams.get('type') as EmailOtpType | null;
-  const next = searchParams.get('next') ?? '/';
+  try {
+    const { searchParams } = new URL(request.url);
+    const token_hash = searchParams.get('token_hash');
+    const type = searchParams.get('type') as EmailOtpType | null;
+    const next = searchParams.get('next') ?? '/';
 
-  if (token_hash && type) {
-    const supabase = await createClient();
+    if (token_hash && type) {
+      const supabase = await createClient();
 
-    const {error} = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next);
+      const { error, data } = await supabase.auth.verifyOtp({
+        type,
+        token_hash,
+      });
+      const authId = data.user?.id
+      if (!error && authId) {
+        await db.update(users)
+          .set({ isVerified: true })
+          .where(eq(users.authId, authId))
+        redirect(next);
+      }
     }
+    redirect('/error');
+  } catch (error: unknown) {
+    console.error('Error confirming email', error instanceof Error && error.message || error)
+    redirect('/error');
   }
-
-  // redirect the user to an error page with some instructions
-  redirect('/error');
 }
--- src/features/Authentication/types.ts
+++ src/features/Authentication/types.ts
@@ -1,4 +1,28 @@
+import { z } from 'zod';
 export type FormState = {
   success: boolean;
   message: string;
 };
+
+const passwordLength = 6;
+
+export const signupFormSchema = z.object({
+  email: z.email({ message: 'Invalid email address' }),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  userType: z.enum(['student', 'professor', 'admin']),
+  bio: z.string().optional(),
+  yearOfStudy: z
+    .number()
+    .min(1, { message: 'Year of study must be at least 1' })
+    .max(5, { message: 'Year of study must be at most 5' })
+    .optional(),
+  password: z
+    .string()
+    .min(
+      passwordLength,
+      { message: `Password must be at least ${passwordLength} characters long` }
+    ),
+})
+
+export type SignupFormData = z.infer<typeof signupFormSchema>;
