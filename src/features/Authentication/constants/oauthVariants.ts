4f78cd2a7bef535ae4c6fc852de12aacc342ba95
Dumitru Postoronca <103861986+DdimaPos@users.noreply.github.com>
2025-09-24 23:08:53 +0300

    Update oauthVariants.ts

--- src/features/Authentication/constants/oauthVariants.ts
+++ src/features/Authentication/constants/oauthVariants.ts
@@ -1,7 +1,7 @@
 import { SignInWithOAuthCredentials } from '@supabase/supabase-js';
 import { ProviderTypes } from '../types';
 
-const CALLBACK_PATH = 'https://www.peerplex-students-forum.vercel.app/api/auth/callback';
+const CALLBACK_PATH = 'https://peerplex-students-forum.vercel.app/api/auth/callback';
 
 export const GOOGLE_AUTH_CONFIGURATION: Readonly<SignInWithOAuthCredentials> = {
   provider: ProviderTypes.GOOGLE,
