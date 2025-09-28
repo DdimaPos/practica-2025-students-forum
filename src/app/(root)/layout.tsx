5980d50da6ced13032cba0246b2ff31f808e0bb5
DdimaPos <postoroncadumitru@gmail.com>
2025-09-28 12:00:18 +0300

    refactor: moved the layout from app level, to (root) level

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,21 @@
+import Navbar from '@/features/navigation';
+import SearchBar from '@/features/SearchBar';
+import { SearchProvider } from '@/features/search/context/SearchContext';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className='flex'>
+      <SearchProvider>
+        <Navbar />
+        <main className='bg-secondary ml-[25%] min-h-screen basis-3/4 p-10'>
+          <SearchBar />
+          {children}
+        </main>
+      </SearchProvider>
+    </div>
+  );
 }
