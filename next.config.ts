44be4a7d41fac2809968c4cd7da56c86492eacca
DimonBel <144515910+DimonBel@users.noreply.github.com>
2025-12-17 00:15:42 +0200

    Add Strict-Transport-Security header

--- next.config.ts
+++ next.config.ts
@@ -66,6 +66,10 @@ const nextConfig: NextConfig = {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
           },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
         ],
       },
     ];
