ddee4d8e1e0a071bcde2d72c6122645e6c206333
DimonBel <144515910+DimonBel@users.noreply.github.com>
2026-01-07 14:44:57 +0200

    Feature/strict transport security header (#149)
    * Add Strict-Transport-Security header
    
    * remove preload form cconfig

--- next.config.ts
+++ next.config.ts
@@ -65,6 +65,10 @@ const nextConfig: NextConfig = {
           {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains;',
           }
         ],
       },
