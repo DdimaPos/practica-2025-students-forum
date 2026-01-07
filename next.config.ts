169eed346b7d7fa4c0a00ed3b04caad20e533f93
DimonBel <144515910+DimonBel@users.noreply.github.com>
2026-01-07 13:48:07 +0200

    remove preload form cconfig

--- next.config.ts
+++ next.config.ts
@@ -68,7 +68,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains; preload',
+            value: 'max-age=31536000; includeSubDomains;',
           }
         ],
       },
