0da3d96f3f6422fdf9df4c014d0a5e0b1133648a
Andre Chicu <andriesh.chicu@gmail.com>
2026-01-14 17:39:44 +0200

    feat(sentry): add native Sentry configuration with enableLogs

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://e583bd42c76ba86edef5271ddc65acf5@o4510709101232128.ingest.de.sentry.io/4510709101690960',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
